f2413cdf88a3abb6dc26ecded21b651ceb8a9d49
Kingsley Amankwah <kuamiklassic149@gmail.com>
2022-11-14 20:36:28 +0000

    Controller

---
+++ backend/controllers/userController.js
@@ -0,0 +1,19 @@
+const createUser = async (req, res) => {
+  res.status(200).json({ message: `Create a user` });
+};
+const getUser = async (req, res) => {
+  res.status(200).json({ message: `Create a user` });
+};
+const updateUser = async (req, res) => {
+  res.status(200).json({ message: `Create a user` });
+};
+const deleteUser = async (req, res) => {
+  res.status(200).json({ message: `Create a user` });
+};
+
+module.exports = {
+  createUser,
+  getUser,
+  updateUser,
+  deleteUser,
+};
