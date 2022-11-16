a2458a4d141c9db1f0b17d866b065b6580bd4ff7
Kingsley Amankwah <kuamiklassic149@gmail.com>
2022-11-16 15:45:46 +0000

    Added UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
-// import Table from "react-bootstrap/Table";
+import Table from "react-bootstrap/Table";
 
 function App() {
   const [formData, setFormData] = useState({
@@ -23,38 +23,58 @@ function App() {
   return (
     <div className="App">
       <Container>
+        <h2 classname="text-center display-4">Details of People</h2>
         <Form>
           <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label>Email address</Form.Label>
+            <Form.Label>Names</Form.Label>
             <Form.Control
               type="text"
               name="Name"
               value={Name}
               onChange={onChange}
-              placeholder="Enter email"
+              placeholder="Enter Name"
             />
-            <Form.Text className="text-muted">
-              We'll never share your email with anyone else.
-            </Form.Text>
           </Form.Group>
 
           <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
+            <Form.Label>Location</Form.Label>
             <Form.Control
               type="text"
               name="Location"
               value={Location}
               onChange={onChange}
-              placeholder="Password"
+              placeholder="Location"
             />
           </Form.Group>
-          <Form.Group className="mb-3" controlId="formBasicCheckbox">
-            <Form.Check type="checkbox" label="Check me out" />
-          </Form.Group>
+
           <Button variant="primary" type="submit">
             Submit
           </Button>
         </Form>
+
+        <Table bordered Stripped className="mt-4">
+          <thead>
+            <tr>
+              <th>No.</th>
+              <th>Name</th>
+              <th>Location</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>Mark</td>
+              <td>@mdo</td>
+              <td>
+                <Button variant="info">Edit</Button>
+                <Button className="ms-2" variant="danger">
+                  Delete
+                </Button>
+              </td>
+            </tr>
+          </tbody>
+        </Table>
       </Container>
     </div>
   );
