acb289ea9aa0d660a4a2ff1db323d2d5a261e76d
Kingsley Amankwah <kuamiklassic149@gmail.com>
2022-11-16 04:24:37 +0000

    Added UI for Forms

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,35 @@
 import React from "react";
 
+import "bootstrap/dist/css/bootstrap.min.css";
+// import { container } from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </div>
+  );
 }
 
 export default App;
