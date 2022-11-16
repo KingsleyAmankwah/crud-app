9caec23ab4a76bc52071ee96aad379c999f6c0c9
Kingsley Amankwah <kuamiklassic149@gmail.com>
2022-11-16 15:26:12 +0000

    Form Data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+// import Table from "react-bootstrap/Table";
 
 function App() {
+  const [formData, setFormData] = useState({
+    name: "",
+    Location: "",
+  });
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const { Name, Location } = formData;
   return (
     <div className="App">
       <Container>
         <Form>
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Control
+              type="text"
+              name="Name"
+              value={Name}
+              onChange={onChange}
+              placeholder="Enter email"
+            />
             <Form.Text className="text-muted">
               We'll never share your email with anyone else.
             </Form.Text>
@@ -20,7 +40,13 @@ function App() {
 
           <Form.Group className="mb-3" controlId="formBasicPassword">
             <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
+            <Form.Control
+              type="text"
+              name="Location"
+              value={Location}
+              onChange={onChange}
+              placeholder="Password"
+            />
           </Form.Group>
           <Form.Group className="mb-3" controlId="formBasicCheckbox">
             <Form.Check type="checkbox" label="Check me out" />
