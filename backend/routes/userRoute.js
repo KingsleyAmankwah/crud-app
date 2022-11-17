666f4467b05be773a22a438b273e725f2070030d
Kingsley Amankwah <kuamiklassic149@gmail.com>
2022-11-17 21:15:39 +0000

    Router changes

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -8,7 +8,7 @@ const {
 const router = express.Router();
 
 router.post("/", createUser);
-router.get("/getMe", getUser);
+router.get("/getUsers", getUser);
 router.delete("/:id", deleteUser);
 router.put("/:id", updateUser);
 
