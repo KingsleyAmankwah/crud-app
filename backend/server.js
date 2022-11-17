0d76a3187a781f98ccb768c45047e6fff2087ac9
Kingsley Amankwah <kuamiklassic149@gmail.com>
2022-11-17 04:32:11 +0000

    Route changes

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use("/api/users/", require("./routes/userRoute"));
+app.use("/api/users", require("./routes/userRoute"));
 
 app.use(errorHandler);
 
