c7a74a6abe4b2b00bfec6153f87580ae73190f2d
Kingsley Amankwah <kuamiklassic149@gmail.com>
2022-11-17 21:18:49 +0000

    useState Removed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Container from "react-bootstrap/Container";
