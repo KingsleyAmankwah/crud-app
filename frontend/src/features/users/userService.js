e968866ad6daa24165f5b61b181ba1491a7a285b
Kingsley Amankwah <kuamiklassic149@gmail.com>
2022-11-17 06:36:04 +0000

    Comment removed

--- frontend/src/features/users/userService.js
+++ frontend/src/features/users/userService.js
@@ -2,8 +2,6 @@ import axios from "axios";
 
 const API_URL = axios.create({ baseURL: "http://localhost:8000" });
 
-// export const createUser = (userData) => API_URL.post("/api/users/", userData);
-
 const createUser = async (userData) => {
   const response = await API_URL.post("/api/users/", userData);
 
