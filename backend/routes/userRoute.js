b6c6d06b62bff22b50ebdf21a033715e2d79f1bd
Kingsley Amankwah <kuamiklassic149@gmail.com>
2022-11-18 23:58:34 +0000

    New route added

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const {
   createUser,
-  getUser,
+  getUsers,
   deleteUser,
   updateUser,
 } = require("../controllers/userController");
 const router = express.Router();
 
 router.post("/", createUser);
-router.get("/getUsers", getUser);
+router.get("/getUser/:id", getUser);
+router.get("/getUsers", getUsers);
 router.delete("/:id", deleteUser);
 router.put("/:id", updateUser);
 
