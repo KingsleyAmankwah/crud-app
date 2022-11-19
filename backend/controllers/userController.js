1add3b2fd59bba7b14181e94ebde05c5868ffb74
Kingsley Amankwah <kuamiklassic149@gmail.com>
2022-11-19 00:19:04 +0000

    get user controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -31,6 +31,16 @@ const getUsers = asyncHandler(async (req, res) => {
   res.status(200).json(users);
 });
 
+const getUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id);
+  if (user) {
+    res.status(200).json(user);
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
+});
+
 const updateUser = asyncHandler(async (req, res) => {
   const user = await User.findById(req.params.id);
 
@@ -60,6 +70,7 @@ const deleteUser = asyncHandler(async (req, res) => {
 module.exports = {
   createUser,
   getUsers,
+  getUser,
   updateUser,
   deleteUser,
 };
