90d82c694d18f64a5943f6a6844a7c42719e0ef5
Kingsley Amankwah <kuamiklassic149@gmail.com>
2022-11-20 04:36:53 +0000

    userTable

--- frontend/src/components/UsersTable.jsx
+++ frontend/src/components/UsersTable.jsx
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Button from "react-bootstrap/Button";
 import Table from "react-bootstrap/Table";
+import { useDispatch, useSelector } from "react-redux";
+import { getUsers } from "../features/users/userSlice";
 
-function UsersTable() {
+function UsersTable({ user }) {
+  // const [users, setUsers] = useState([]);
+  const { profile } = useSelector((state) => state.user);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getUsers());
+  }, [dispatch]);
+
   return (
     <div>
       <Table bordered className="mt-4">
@@ -11,23 +22,29 @@ function UsersTable() {
             <th>No.</th>
             <th>Name</th>
             <th>Location</th>
-            <th>Date</th>
+
             <th>Action</th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>ID</td>
-            <td>Name</td>
-            <td>Location</td>
-            <td>Date</td>
-            <td>
-              <Button variant="info">Edit</Button>
-              <Button className="ms-2" variant="danger">
-                Delete
-              </Button>
-            </td>
-          </tr>
+          {profile &&
+            profile.map((user, index) => {
+              const { _id, Name, Location } = user;
+
+              return (
+                <tr key={index}>
+                  <td>{_id}</td>
+                  <td>{Name}</td>
+                  <td>{Location}</td>
+                  <td>
+                    <Button variant="info">Edit</Button>
+                    <Button className="ms-2" variant="danger">
+                      Delete
+                    </Button>
+                  </td>
+                </tr>
+              );
+            })}
         </tbody>
       </Table>
     </div>
