b98960759c0cec6ede101ecdd06655159ff096c9
Kingsley Amankwah <kuamiklassic149@gmail.com>
2022-11-20 02:10:35 +0000

    Clean up

--- frontend/src/components/UsersTable.jsx
+++ frontend/src/components/UsersTable.jsx
@@ -1,18 +1,8 @@
-// import React from "react";
-import React, { useEffect } from "react";
+import React from "react";
 import Button from "react-bootstrap/Button";
 import Table from "react-bootstrap/Table";
-import { useDispatch, useSelector } from "react-redux";
-import { getUsers } from "../features/users/userSlice";
+
 function UsersTable() {
-  const { users } = useSelector((state) => ({ ...state.user }));
-
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(getUsers());
-  }, [dispatch]);
-
   return (
     <div>
       <Table bordered className="mt-4">
@@ -21,25 +11,24 @@ function UsersTable() {
             <th>No.</th>
             <th>Name</th>
             <th>Location</th>
+            <th>Date</th>
             <th>Action</th>
           </tr>
         </thead>
-        {users &&
-          users.map((user) => (
-            <tbody key={user._id}>
-              <tr>
-                <td>{user.Name}</td>
-                <td>{user.Location} </td>
-                <td>{new Date(user.createdAt).toLocaleString("en-US")}</td>
-                <td>
-                  <Button variant="info">Edit</Button>
-                  <Button className="ms-2" variant="danger">
-                    Delete
-                  </Button>
-                </td>
-              </tr>
-            </tbody>
-          ))}
+        <tbody>
+          <tr>
+            <td>ID</td>
+            <td>Name</td>
+            <td>Location</td>
+            <td>Date</td>
+            <td>
+              <Button variant="info">Edit</Button>
+              <Button className="ms-2" variant="danger">
+                Delete
+              </Button>
+            </td>
+          </tr>
+        </tbody>
       </Table>
     </div>
   );
