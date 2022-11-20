6ae48eb51e08439c7155be9a839bb57a9e3e20f6
Kingsley Amankwah <kuamiklassic149@gmail.com>
2022-11-20 02:12:42 +0000

    Clean up

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,6 @@
 import React from "react";
-// import React, { useEffect } from "react";
-// import { useDispatch, useSelector } from "react-redux";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Container from "react-bootstrap/Container";
-
-// import { getUsers } from "./features/users/userSlice";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import UsersForm from "./components/UsersForm";
