fa953bb9173147477c212ab07e9736ff047a3992
Kingsley Amankwah <kuamiklassic149@gmail.com>
2022-11-21 22:45:27 +0000

    Code refactored

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,12 @@
-import React, { useEffect } from "react";
+import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Container from "react-bootstrap/Container";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import UsersForm from "./components/UsersForm";
-import { useDispatch } from "react-redux";
-import { getUsers } from "./features/users/userSlice";
 import UserTableData from "./components/UserTableData";
 
 function App() {
-  const dispatch = useDispatch();
-
-
-  useEffect(() => {
-    dispatch(getUsers());
-  }, [dispatch]);
-
   return (
     <div className="App">
       <Container>
