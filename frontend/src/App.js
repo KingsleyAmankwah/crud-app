3b67ee9240cc72f3d7f6b78b343668b3d07074ac
Kingsley Amankwah <kuamiklassic149@gmail.com>
2023-02-13 03:27:18 +0000

    Clean Up

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,12 @@
 import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import Container from "react-bootstrap/Container";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import UsersForm from "./components/UsersForm";
-import UserTableData from "./components/UserTableData";
 
 function App() {
   return (
     <div className="App">
-      <Container>
-        <UsersForm />
-        <UserTableData />
-        <ToastContainer />
-      </Container>
+      <h1>Hello</h1>
+      <ToastContainer />
     </div>
   );
 }
--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,8 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import userReducer from "../features/users/userSlice";
 
 export const store = configureStore({
-  reducer: {
-    user: userReducer,
-  },
+  reducer: {},
 });
