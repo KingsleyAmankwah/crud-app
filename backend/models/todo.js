d21de2f9c6c4412cf0467ee421648464c2a744a4
Kingsley Amankwah <kuamiklassic149@gmail.com>
2023-02-16 10:15:22 +0000

    todo model

--- backend/models/todo.js
+++ backend/models/todo.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const taskSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  dueDate: { type: Date, required: true },
+  dueDate: { type: String, required: true },
   description: { type: String, required: true },
   completed: { type: Boolean, default: false },
 });
