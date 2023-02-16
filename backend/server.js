e277536339b3d3bd7417a40fcba67730313d9495
Kingsley Amankwah <kuamiklassic149@gmail.com>
2023-02-16 10:14:52 +0000

    Server Entry file

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,12 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    credentials: true,
+  })
+);
 
 app.use("/todo", require("./routes/todoRoutes"));
 
