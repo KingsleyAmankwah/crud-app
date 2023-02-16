9c3cd1ad239e1099f1b056da69a5fad3d6245255
Kingsley Amankwah <kuamiklassic149@gmail.com>
2023-02-16 10:23:43 +0000

    TodoForm

--- frontend/src/components/TodoForm.jsx
+++ frontend/src/components/TodoForm.jsx
@@ -64,7 +64,7 @@ const TodoListForm = () => {
 
   return (
     <>
-      <div>
+      <div className="mt-3">
         <Link
           to="/"
           className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded"
