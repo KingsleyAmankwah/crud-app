1dac2a949f028268444fe3750a2f88c9d078202d
Kingsley Amankwah <kuamiklassic149@gmail.com>
2023-02-16 10:16:09 +0000

    App Store

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,5 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import todoReducer from "../features/todo/todoSlice";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    todo: todoReducer,
+  },
 });
