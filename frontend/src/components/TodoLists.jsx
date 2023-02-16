f6ba7892d1dfc95d7209de84b2b81ab11167c57b
Kingsley Amankwah <kuamiklassic149@gmail.com>
2023-02-16 10:23:53 +0000

    TodoList

--- frontend/src/components/TodoLists.jsx
+++ frontend/src/components/TodoLists.jsx
@@ -87,7 +87,7 @@ const TodoLists = () => {
 
   return (
     <>
-      <div>
+      <div className="mt-3">
         <Link
           to="/"
           className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded"
